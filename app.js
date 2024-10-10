d8cc6c41cb66db312db59f78843b789ec309997e
NesoDev <ever.avendano@unmsm.edu.pe>
2024-10-10 18:54:09 -0500

    fourthy commit

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-console.log('importando modulos')
-import createTagsQuestions from "./question.js";
-import sendAudio from "./api.js";
-console.log('modulos importados')
 let state = false;
 let mediaRecorder;
 let audioChunks = [];
@@ -61,5 +57,43 @@ const toggleRecording = () => {
     stopRecording();
   }
 };
+
+const sendAudio = async (file) => {
+  const response = await fetch('https://goldfish-app-kfo84.ondigitalocean.app/upload', {
+      method: 'POST',
+      body: new FormData().append('audio', file),
+  });
+
+  if (response.ok) {
+      const data = await response.json();
+      console.log('Audio enviado:', data);
+      return data;
+  } else {
+      console.error('Error al enviar el audio:', response.status);
+  }
+};
+
+const createTagQuestion = (text) => {
+  const div = document.createElement('div');
+  div.textContent = text;
+  div.style.display = 'flex';
+  div.style.justifyContent = 'center';
+  div.style.alignItems = 'center';
+  div.style.height = '100px';
+  div.style.border = '1px solid gray';
+  div.style.backgroundColor = '#ffff';
+  div.style.margin = '10px 0';
+  return div;
+};
+
+const createTagsQuestions = (container, data) => {
+  console.log('creando tags')
+  questions = data.transcribed_text
+  Object.values(questions).forEach(question => {
+      let tag = createTagQuestion(question);
+      container.appendChild(tag)
+  });
+  console.log('tags creadas')
+}
 
 button.addEventListener('click', toggleRecording);
